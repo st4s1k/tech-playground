ed864ad1b06aead6ee1e97b2687a35d2af85d7a0
Stanislav Girlea <Stanislav.Girlea@endava.com>
2025-02-19 17:11:18 +0200

    first commit

---
+++ src/main/java/com/st4s1k/techplayground/TechPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.st4s1k.techplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechPlaygroundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechPlaygroundApplication.class, args);
+	}
+
+}
